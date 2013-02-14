681e22e1bd96935a580d16c7f4a73d78fbc6a261
Alberto La Rocca <a71104@gmail.com>
2013-02-14 20:47:54 +0100

    doc stubs.

--- src/ReadWriteLock.js
+++ src/ReadWriteLock.js
@@ -1,8 +1,23 @@
+/**
+ * TODO
+ *
+ * @class ReadWriteLock
+ * @constructor
+ */
 module.exports = function ReadWriteLock() {
 	'use strict';
 
 	var table = {};
 
+	/**
+	 * TODO
+	 *
+	 * @method readLock
+	 * @param key {String} TODO
+	 * @param callback {Function} TODO
+	 * @param callback.release {Function} TODO
+	 * @param [timeout] {Number} TODO
+	 */
 	function readLock(key, callback) {
 		var release = (function () {
 			var released = false;
@@ -38,6 +53,15 @@ module.exports = function ReadWriteLock() {
 		}
 	}
 
+	/**
+	 * TODO
+	 *
+	 * @method writeLock
+	 * @param key {String} TODO
+	 * @param callback {Function} TODO
+	 * @param callback.release {Function} TODO
+	 * @param [timeout] {Number} TODO
+	 */
 	function writeLock(key, callback) {
 		var release = (function () {
 			var released = false;
@@ -69,6 +93,14 @@ module.exports = function ReadWriteLock() {
 	this.readLock = readLock;
 	this.writeLock = writeLock;
 
+	/**
+	 * TODO
+	 *
+	 * @method criticalSection
+	 * @param key {String} TODO
+	 * @param callback {Function} TODO
+	 * @param [timeout] {Number} TODO
+	 */
 	this.criticalSection = function (key, callback, timeout) {
 		writeLock(key, function (release) {
 			callback();
